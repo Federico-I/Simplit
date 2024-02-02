8e30a36e8bbf55f0bb394607916ea832ee182226
Federico-I <threxos.48@gmail.com>
2024-02-02 22:37:23 +0000

    project-setup

--- src/App.js
+++ src/App.js
@@ -1,100 +1,52 @@
-import { useState } from "react";
-
-const content = [
+const initialFriends = [
   {
-    summary: "React is a library for building UIs",
-    details:
-      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
   },
   {
-    summary: "State management is like giving state a home",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
   },
   {
-    summary: "We can think of props as the component API",
-    details:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
   },
 ];
 
+
 export default function App() {
-  return (
-    <div>
-      <Tabbed content={content} />
-    </div>
-  );
-}
-
-function Tabbed({ content }) {
-  const [activeTab, setActiveTab] = useState(0);
-
-  return (
-    <div>
-      <div className="tabs">
-        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
-      </div>
-
-      {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
-      ) : (
-        <DifferentContent />
-      )}
-    </div>
-  );
-}
-
-function Tab({ num, activeTab, onClick }) {
-  return (
-    <button
-      className={activeTab === num ? "tab active" : "tab"}
-      onClick={() => onClick(num)}
-    >
-      Tab {num + 1}
-    </button>
-  );
-}
-
-function TabContent({ item }) {
-  const [showDetails, setShowDetails] = useState(true);
-  const [likes, setLikes] = useState(0);
-
-  function handleInc() {
-    setLikes(likes + 1);
-  }
-
-  return (
-    <div className="tab-content">
-      <h4>{item.summary}</h4>
-      {showDetails && <p>{item.details}</p>}
-
-      <div className="tab-actions">
-        <button onClick={() => setShowDetails((h) => !h)}>
-          {showDetails ? "Hide" : "Show"} details
-        </button>
-
-        <div className="hearts-counter">
-          <span>{likes} ❤️</span>
-          <button onClick={handleInc}>+</button>
-          <button>+++</button>
-        </div>
-      </div>
-
-      <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+  return(
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
       </div>
     </div>
-  );
-}
+  )
+};
 
-function DifferentContent() {
-  return (
-    <div className="tab-content">
-      <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
-    </div>
-  );
+function FriendsList() {
+const friends = initialFriends;
+
+  return(
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id}/>
+      ))}
+    </ul>
+  )
+};
+
+function Friend({ friend }) {
+  return(
+    <li>
+      <image src={friend.image} alt={friend.name}/>
+      <h3>{friend.name}</h3>
+    </li>
+  )
 }
