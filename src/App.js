3005478769ba1192d75374f97f0331f14bb13296
Federico-I <threxos.48@gmail.com>
2024-02-02 22:47:48 +0000

    friends-component

--- src/App.js
+++ src/App.js
@@ -47,6 +47,9 @@ function Friend({ friend }) {
     <li>
       <image src={friend.image} alt={friend.name}/>
       <h3>{friend.name}</h3>
+      {friend.balance < 0 && (<p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>)}
+      {friend.balance < 0 && (<p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>)}
+      {friend.balance < 0 && (<p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>)}
     </li>
   )
 }
