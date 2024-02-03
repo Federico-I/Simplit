97b6b6177714efa1f6591ea6baa23de35c7fd516
Federico-I <threxos.48@gmail.com>
2024-02-03 20:47:43 +0000

    simplit-mainComponents

--- src/App.js
+++ src/App.js
@@ -25,10 +25,19 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <Button>Add Friend</Button>
+        <FormAddFriend />
+        <FormSplitBill />
       </div>
     </div>
   )
 };
+
+function Button({ children }) {
+  return(
+    <button className="button">children</button>
+  )
+}
 
 function FriendsList() {
 const friends = initialFriends;
@@ -50,6 +59,46 @@ function Friend({ friend }) {
       {friend.balance < 0 && (<p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>)}
       {friend.balance > 0 && (<p className="green">{friend.name} owes you {Math.abs(friend.balance)}$</p>)}
       {friend.balance === 0 && (<p className="red">You and {friend.name} {Math.abs(friend.balance)}$</p>)}
+      <Button>Select</Button>
     </li>
+  )
+}
+
+function FormAddFriend() {
+  return (
+    <form>
+      <label>Friends Name</label>
+      <input type="text"/>
+
+      <label>Image Url</label>
+      <input type="text"/>
+
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+function FormSplitBill({ friendsName }){
+  return(
+    <form className="form-split-bill">
+      <h2>Split a bill with {friendsName}</h2>
+
+      <label>Total bill</label>
+      <input type="text"/>
+
+      <label>Your Expenses</label>
+      <input type="text"/>
+
+      <label>Friend Expenses</label>
+      <input type="text" disabled/>
+
+      <label>Who's paying?</label>
+      <select>
+        <option value={user}>You</option>
+        <option value={friend}>Friend</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   )
 }
